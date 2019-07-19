fb5d9081590df6dcbd8b8f3e01214aacb8276ec6
DylanCassidy <djcassidy2002@outlook.com>
2019-07-19 16:34:36 -0400

    Added physics, player functionality, and goals.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody rb = new Rigidbody();
+    [SerializeField] public float xSpeed = 10;
+    [SerializeField] public float ySpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        gameStart();
     }
-
+    public void gameStart()
+    {
+        //rb.velocity = Vector3.zero;
+        transform.position = new Vector3(0, 0, 0);
+        rb.velocity = new Vector3(0, 0, 0);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && rb.velocity == Vector3.zero)
+        {
+            rb.velocity = new Vector3(xSpeed + Random.Range(-1f, 1f), ySpeed + Random.Range(-1f, 1f), 0);
+        }
     }
-    public void BallScored()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        rb.velocity = new Vector3(rb.velocity.x + Random.Range(-1f, 1f), rb.velocity.y + Random.Range(-1f, 1f), 0);
     }
 }
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    //Ball ball = new Ball();
+    [SerializeField]
+    private Ball ball;
+    public enum GoalNumber { Goal1, Goal2 };
+    public GoalNumber goalNumber = GoalNumber.Goal1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (goalNumber == GoalNumber.Goal1)
+        {
+            Debug.Log("P2 Score");
+            ball.xSpeed = -10;
+            ball.gameStart();
+        }
+
+        if (goalNumber == GoalNumber.Goal2)
+        {
+            Debug.Log("P1 Score");
+            ball.xSpeed = 10;
+            ball.gameStart();
+        }
+    }
+}
